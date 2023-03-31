fcde1eb3b8b00bf113bb19952ecce499b0a86b92
marcoscutaro <marcostruggia@gmail.com>
2023-03-31 12:10:19 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,55 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const newCard = document.getElementById("btnId");
+const widthInput = document.getElementById("widthInput");
+const heightInput = document.getElementById("heightInput");
+const card = document.querySelector(".card");
+const number = document.querySelector(".number");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let currentSuitClass = "diam";
+
+window.onload = () => {
+  updateCard();
+  newCard.addEventListener("click", updateCard);
+  const intervalID = setInterval(updateCard, 4000);
+};
+
+const generateRandomNumber = () => {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  const index = Math.floor(Math.random() * numbers.length);
+  return numbers[index];
+};
+
+const generateRandomSuit = () => {
+  const suits = ["diam", "spade", "heart", "club"];
+  const index = Math.floor(Math.random() * suits.length);
+  const suit = suits[index];
+  console.log("generated suit:", suit);
+  return suit;
+};
+
+const updateCard = () => {
+  card.style.width = `${widthInput.value}px`;
+  card.style.height = `${heightInput.value}px`;
+  //card.className = "card " + generateRandomSuit();
+  card.classList.remove(currentSuitClass);
+  currentSuitClass = generateRandomSuit();
+  card.classList.add(currentSuitClass);
+  //card.classList.add(generateRandomSuit());
+  number.innerHTML = generateRandomNumber();
 };
